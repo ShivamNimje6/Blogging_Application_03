e8126b8409202ae0fefec09e5e337165b5329456
ShivamNimje6 <shivamnimje6@gmail.com>
2024-07-06 19:24:05 +0530

    Ready to deploy

---
+++ routes/posts.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const verifyToken = require("../verifyToken");
+
+const calculateReadingTime = (content) => {
+  const wordsPerMinute = 200;
+  const words = content.split(/\s+/).length;
+  const time = Math.ceil(words / wordsPerMinute);
+  return `${time} min read`;
+};
+
+// CREATE
+router.post("/create", verifyToken, async (req, res) => {
+  try {
+    const { title, desc, photo, username, userId, categories } = req.body;
+    const readingTime = calculateReadingTime(desc);
+    console.log(" reading time", readingTime);
+    const newPost = new Post({
+      title,
+      desc,
+      photo,
+      username,
+      userId,
+      categories,
+      readingTime,
+    });
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    console.log(" reading time", readingTime);
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const { title, desc, photo, username, userId, categories } = req.body;
+    const readingTime = calculateReadingTime(desc);
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: { title, desc, photo, username, userId, categories, readingTime },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    await Comment.deleteMany({ postId: req.params.id });
+    res.status(200).json("Post has been deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET POST DETAILS
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET POSTS
+router.get("/", async (req, res) => {
+  const query = req.query;
+
+  try {
+    const searchFilter = {
+      title: { $regex: query.search, $options: "i" },
+    };
+    const posts = await Post.find(query.search ? searchFilter : null);
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER POSTS
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const posts = await Post.find({ userId: req.params.userId });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

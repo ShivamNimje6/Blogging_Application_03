395f0d63ef33db14e150f34eae32588dd77474f2
ShivamNimje6 <shivamnimje6@gmail.com>
2024-07-07 11:56:26 +0530

    done

---
+++ frontend/src/url.js
@@ -0,0 +1,5 @@
+// export const URL = "http://localhost:5000";
+// export const IF = "http://localhost:5000/images/";
+
+export const URL = "https://blogging-application-ib3v.onrender.com";
+export const IF = "https://blogging-application-ib3v.onrender.com/images/";
